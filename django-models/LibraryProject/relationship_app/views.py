183672ad6c31ee48fdeb8437ce5b7d1e4277d4bc
Rebi Edward Akpa <rebifaizahedwardakpa@gmail.com>
2026-01-18 07:17:11 +0100

    Add Library import and correct template path for LibraryDetailView

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,16 +1,17 @@
 from django.shortcuts import render
 from django.views.generic import DetailView
-from .models import Book, Library
+from .models import Book, Library  # <-- Library MUST be explicitly imported
 
-# Function-based view: list all books
+# Function-based view
 def list_books(request):
     books = Book.objects.all()
     return render(request, "relationship_app/list_books.html", {"books": books})
 
 
-# Class-based view: library details
+# Class-based view
 class LibraryDetailView(DetailView):
     model = Library
-    template_name = "library_detail.html"
+    template_name = "relationship_app/library_detail.html"  # <-- REQUIRED
     context_object_name = "library"
+
 
