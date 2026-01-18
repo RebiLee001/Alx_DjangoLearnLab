b5dbc9eff2f79dee81ccb92cf876fbbbb10f4ea6
Rebi Edward Akpa <rebifaizahedwardakpa@gmail.com>
2026-01-18 06:54:49 +0100

    Add sample ORM queries for model relationships

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,24 +1,17 @@
-# query_samples.py
 from relationship_app.models import Author, Book, Library, Librarian
 
-# 1. Create sample data
-author1 = Author.objects.create(name="George Orwell")
-book1 = Book.objects.create(title="1984", author=author1)
-book2 = Book.objects.create(title="Animal Farm", author=author1)
+# Query all books by a specific author
+author_name = "George Orwell"
+author = Author.objects.get(name=author_name)
+books_by_author = Book.objects.filter(author=author)
+print(books_by_author)
 
-library1 = Library.objects.create(name="Central Library")
-library1.books.add(book1, book2)
+# List all books in a library
+library_name = "Central Library"
+library = Library.objects.get(name=library_name)
+books_in_library = library.books.all()
+print(books_in_library)
 
-librarian1 = Librarian.objects.create(name="Alice Johnson", library=library1)
-
-# 2. Query all books by a specific author
-books_by_orwell = Book.objects.filter(author__name="George Orwell")
-print("Books by George Orwell:", list(books_by_orwell))
-
-# 3. List all books in a library
-books_in_library = library1.books.all()
-print("Books in Central Library:", list(books_in_library))
-
-# 4. Retrieve the librarian for a library
-library_librarian = library1.librarian
-print("Librarian of Central Library:", library_librarian)
+# Retrieve the librarian for a library
+librarian = Librarian.objects.get(library=library)
+print(librarian)
