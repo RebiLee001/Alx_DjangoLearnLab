cfb92c1427cca709d3649252aceaa7555638cbc5
Rebi Edward Akpa <rebifaizahedwardakpa@gmail.com>
2026-01-25 16:22:59 +0100

    Fix models.py imports and add custom permissions to Book model

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import permission_required
+from .models import Book
 
-# Create your views here.
+# View for listing books (requires can_view permission)
+@permission_required('bookshelf.can_view', raise_exception=True)
+def book_list(request):
+    books = Book.objects.all()
+    return render(request, 'bookshelf/book_list.html', {'books': books})
+
+# Example view for creating a book (requires can_create permission)
+@permission_required('bookshelf.can_create', raise_exception=True)
+def book_create(request):
+    # You can implement a form here later
+    return render(request, 'bookshelf/book_create.html')
