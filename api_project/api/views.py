176a55743d79cf16d8144490e2de374d78f0904f
Rebi Edward Akpa <rebifaizahedwardakpa@gmail.com>
2026-02-01 06:12:09 +0100

    Add Book list API endpoint using Django REST Framework

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,17 +1,16 @@
-from rest_framework import generics
-from .models import Book
 from rest_framework import generics, viewsets
+from rest_framework.permissions import IsAuthenticated
 from .models import Book
 from .serializers import BookSerializer
 
 
-# Existing ListAPIView (DO NOT REMOVE)
 class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]
 
 
-# New ViewSet for full CRUD operations
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]
