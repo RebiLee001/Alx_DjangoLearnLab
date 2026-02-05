037a0e588f0402c473a4e6f73b7eacf480653f30
Rebi Edward Akpa <rebifaizahedwardakpa@gmail.com>
2026-02-05 08:41:34 +0100

    Include api.urls in project URL configuration

--- advanced-api-project/advanced_api_project/settings.py
+++ advanced-api-project/advanced_api_project/settings.py
@@ -38,11 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
- # Third-party apps
     'rest_framework',
+    'django_filters',
 
-    # Local apps
-    'api',
+    'api'
 ]
 
 MIDDLEWARE = [
