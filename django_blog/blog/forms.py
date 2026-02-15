f35f0970f9ab3bea6ea4762cde0f8e8ac8196602
Rebi Edward Akpa <rebifaizahedwardakpa@gmail.com>
2026-02-15 07:15:03 +0100

    Initial Django blog setup with authentication and CRUD for posts

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -1,25 +1,9 @@
 from django import forms
 from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserCreationForm
 
-
-class CustomUserCreationForm(UserCreationForm):
+class UserUpdateForm(forms.ModelForm):
     email = forms.EmailField(required=True)
 
     class Meta:
         model = User
-        fields = ("username", "email", "password1", "password2")
-
-
-class UserUpdateForm(forms.ModelForm):
-    class Meta:
-        model = User
-        fields = ("username", "email")
-
-class CommentForm(forms.ModelForm):
-    class Meta:
-        model = Comment
-        fields = ['content']
-        widgets = {
-            'content': forms.Textarea(attrs={'rows': 3, 'placeholder': 'Add a comment...'})
-        }
+        fields = ['username', 'email', 'first_name', 'last_name']
