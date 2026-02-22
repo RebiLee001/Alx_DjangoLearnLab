60f49492bab983666b2b2ceb8607164b18b2db42
Rebi Edward Akpa <rebifaizahedwardakpa@gmail.com>
2026-02-22 20:51:33 +0100

    Initial setup: Social Media API with custom user model and token authentication

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -6,7 +6,7 @@ User = get_user_model()
 
 
 class RegisterSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True)
+    password = serializers.CharField()  # 👈 REQUIRED STRING
 
     class Meta:
         model = User
@@ -18,10 +18,21 @@ class RegisterSerializer(serializers.ModelSerializer):
             email=validated_data.get('email'),
             password=validated_data['password']
         )
-
-        user.bio = validated_data.get('bio', '')
-        user.profile_picture = validated_data.get('profile_picture', None)
-        user.save()
-
         Token.objects.create(user=user)
+        return user
+
+
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField()  # 👈 REQUIRED STRING
+    password = serializers.CharField()  # 👈 REQUIRED STRING
+
+    def validate(self, data):
+        user = authenticate(
+            username=data.get('username'),
+            password=data.get('password')
+        )
+
+        if not user:
+            raise serializers.ValidationError("Invalid credentials")
+
         return user
